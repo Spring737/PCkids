aa137e16c15e8754bcb66109c663fbeeb9d8b968
liaoliaololo <852527372@qq.com>
2017-09-12 21:35:30 +0800

    第一次提交

--- pckids/src/app/app.module.ts
+++ pckids/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { AppComponent } from './root/app.component';
+import { ParentingComponent } from './parenting/parenting.component';
+import { ArticleDetailComponent } from './parenting/article-detail/article-detail.component';
+import { QuestionDetailComponent } from './parenting/question-detail/question-detail.component';
+import { QuestionComponent } from './parenting/question/question.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations:[
+    AppComponent,
+    ParentingComponent,
+    ArticleDetailComponent,
+    QuestionDetailComponent,
+    QuestionComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [QuestionComponent]
 })
 export class AppModule { }
